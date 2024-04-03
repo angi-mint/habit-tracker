cb13611a12c9340c18533e0040b1b5e0b276c593
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-03 09:29:21 +0200

    created vite config files
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ vite/vite.renderer.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { pluginExposeRenderer } from './vite.base.config.mjs';
+import vuePlugin from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  /** @type {import('vite').ConfigEnv<'renderer'>} */
+  const forgeEnv = env;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? '';
+
+  /** @type {import('vite').UserConfig} */
+  return {
+    root,
+    mode,
+    base: './',
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [pluginExposeRenderer(name), vuePlugin()],
+    resolve: {
+      preserveSymlinks: true,
+    },
+    clearScreen: false,
+  };
+});
