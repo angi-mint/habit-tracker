5a05aadf4c71ef2caaae8f670fb80a033d93f1d0
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-03 22:54:44 +0200

    created root component for vue
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/vue/App.vue
@@ -0,0 +1,19 @@
+<!--This is the root vue file
+Import components like this:
+import HelloWorld from './components/HelloWorld.vue'
+(no need to export components; the name of the component is the name of the file, excluding the .vue extension)
+To use components:
+<HelloWorld />
+-->
+
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Habit Tracker</h1>
+  </main>
+</template>
+
+<style scoped>
+</style>
