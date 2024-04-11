3e11cc9c3a645296d8c4a174fcc3551f960d06cb
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-11 22:11:39 +0200

    added label component
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/vue/components/form/LabelForm.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps({
+    required: Boolean,
+})
+</script>
+
+<template>
+    <label class="form-label">
+        <slot name="form-label"></slot>
+        <p class="form-required" v-if="props.required">*</p>
+    </label>
+    <slot name="input"></slot>
+</template>
+
+<style scoped>
+
+</style>S
