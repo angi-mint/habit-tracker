f5cfdc2c71fbd7f55fe836fce2e6fc7e22fa7dd8
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-15 12:55:27 +0200

    added interval and time range inputs
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/CreateHabit.vue
+++ src/vue/components/CreateHabit.vue
@@ -21,6 +21,7 @@ interface HabitProps {
 const habitName = ref('');
 const habitCategory = ref('');
 const habitIcon = ref('');
+const habitInterval = ref('');
 
 const onSubmit = () => {
     const formData: HabitProps = {
@@ -49,15 +50,25 @@ const categories: Ref<Array<DatabaseList>> = computed(() => {
 const icons: Ref<Array<DatabaseList>> = computed(() => {
     const icon = [
         { pk: 1, value: "heart" },
-    ]
+    ];
 
     return icon;
-})
+});
+
+const intervals: Ref<Array<DatabaseList>> = computed(() => {
+    const interval = [
+        { pk: 1, value: "täglich" },
+        { pk: 2, value: "wöchentlich" },
+        { pk: 3, value: "zweiwöchentlich" },
+        { pk: 4, value: "monatlich" },
+    ];
+    return interval;
+});
 
 </script>
 
 <template>
-    <button @click="open = true">Neues Habit</button>
+    <button class="btn-habit" @click="open = true">Neues Habit</button>
     <Teleport to="body">
         <div v-if="open" class="modal">
             <div class="modal-content">
@@ -68,14 +79,14 @@ const icons: Ref<Array<DatabaseList>> = computed(() => {
                     <LabelForm required>
                         <template v-slot:form-label><p>Name</p></template>
                         <template v-slot:input>
-                            <input class="form-input" type="text" v-model="habitName" placeholder="Wie soll dein Habit heißen?"/>
+                            <input class="form-input" type="text" v-model="habitName" placeholder="z.B. Wasser trinken" required/>
                         </template>
                     </LabelForm>
 
                     <LabelForm>
-                        <template #form-label>Kategorie</template>
+                        <template #form-label><p>Kategorie</p></template>
                         <template #input>
-                            <select v-if="categories.length > 0" v-model="habitCategory">
+                            <select class="form-select" v-if="categories.length > 0" v-model="habitCategory">
                                 <option disabled value=""> Wähle eine Kategorie</option>
                                 <option v-for="items in categories" :value="items.pk">
                                     {{ items.value }}
@@ -86,12 +97,46 @@ const icons: Ref<Array<DatabaseList>> = computed(() => {
                     </LabelForm>
 
                     <LabelForm>
-                        <template #form-label>Icon</template>
+                        <template #form-label><p>Icon</p></template>
                         <template #input>
                             <label v-for="items in icons">
                                 <input type="radio" v-model="habitIcon" :value=items.pk />
                                 <HabitIcon :id=items.value />
                             </label>
+                        </template>
+                    </LabelForm>
+
+                    <LabelForm required>
+                        <template #form-label><p>Intervall</p></template>
+                        <template #input>
+                            <select class="form-select" v-model="habitInterval">
+                                <option disabled value="">Wähle ein Intervall</option>
+                                <option v-for="items in intervals" :value="items.pk">
+                                    {{ items.value }}
+                                </option>
+                            </select>
+                        </template>
+                    </LabelForm>
+
+                    <LabelForm required>
+                        <template #form-label><p>Häufigkeit</p></template>
+                        <template #input>
+                            <input class="form-input form-number" type="number" value="1" min="1" v-model="habitFrequency" required>
+                        </template>
+                    </LabelForm>
+
+                    <LabelForm>
+                        <template #form-label><p>Zeitraum</p></template>
+                        <template #input>
+                            <div class="form-input form-label">
+                                <label class="toggler-wrapper style-1">
+                                    <input type="checkbox">
+                                    <div class="toggler-slider">
+                                        <div class="toggler-knob"></div>
+                                    </div>
+                                </label>
+                                <input type="date"><input type="date">
+                            </div>
                         </template>
                     </LabelForm>
                     <input type="submit">
