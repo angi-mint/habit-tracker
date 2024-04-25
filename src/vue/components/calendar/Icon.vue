756ef49255905e732dbdcd36b555f44bdf26e111
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-25 15:34:58 +0200

    Icon Component for Habit Overview
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/vue/components/calendar/Icon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HabitIcon from "../icons/HabitIcon.vue";
+
+const IconState = defineProps({
+    id: String,
+    color: String,
+});
+
+</script>
+
+<template>
+    <HabitIcon :id="IconState.id!" :color="IconState.color" :size='40' ></HabitIcon>
+</template>
+
+<style scoped>
+
+</style>
