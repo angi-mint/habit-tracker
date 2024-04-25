a2fbe847286b6a96799a9f486f1fe49144b7c8fe
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-25 12:39:49 +0200

    Button Component created
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/vue/components/calendar/Button.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+const HabitTrack = defineProps({
+    id: Number,
+    name: String,
+    color: String,
+    frequency: Number,
+    entries: Number
+});
+
+async function trackHabit() {
+    //await window.api.sendTrackHabit(HabitTrack.id);
+    console.log(HabitTrack.id)
+}
+
+</script>
+
+<template>
+    <button @click="trackHabit" class="btn-tracker">
+    </button>
+</template>
+
+<style scoped>
+button {
+    background-color: v-bind('HabitTrack.color');
+}
+</style>
