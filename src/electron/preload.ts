913864775fa6172bd616999f8dd2ad80993a5bcc
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-25 16:42:09 +0200

    fixes and code improvements
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,13 +1,13 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 
-const { contextBridge, ipcRenderer } = require('electron');
+import {contextBridge, ipcRenderer} from 'electron';
 
 const API = {
 
     sendMsg: (msg: any) => ipcRenderer.send("message-test", msg),
 
-    onCount: (callback: any) => ipcRenderer.on("count", (event: any, args: any) => {
+    onCount: (callback: any) => ipcRenderer.on("count", (_event: any, args: any) => {
         callback(args);
     }),
     sendPromise: (msg: any) => ipcRenderer.invoke("promise-msg", msg),
