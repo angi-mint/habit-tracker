31dae4eb3d2fb50cb95b0c4dc140f9f438863a45
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-25 16:25:53 +0200

    Comment for Database
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/calendar/DailyOverview.vue
+++ src/vue/components/calendar/DailyOverview.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
 import DailyTracker from "./DailyTracker.vue";
 
-// get actual data  from Database later
-const data = [
-        [{id: 1, name: "Wasser trinken", icon: "heart", color: "#e97a7a", frequency: 5, entries: 3},
-            {id: 2, name: "Buch lesen", icon: "pill", color: "#be4848", frequency: 4, entries: 1}],
-        [{id: 3, name: "Vitamine nehmen", icon: "book", color: "#be4848", frequency: 10, entries: 0},
-            {id: 4, name: "Wasser trinken", icon: "glass", color: "#912929", frequency: 5, entries: 3}],
-        [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#983f3f", frequency: 5, entries: 2},
-            {id: 6, name: "Sport machen", icon: "pill", color: "#d28787", frequency: 4, entries: 2}],
-        [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#be4848", frequency: 5, entries: 5},
-            {id: 6, name: "Sport machen", icon: "pill", color: "#be4848", frequency: 4, entries: 4}]
-]
+// get actual data from database later
+const data = [[{id: 1, name: "Wasser trinken", icon: "heart", color: "#e97a7a", frequency: 5, entries: 3}, {id: 2, name: "Buch lesen", icon: "pill", color: "#be4848", frequency: 4, entries: 1}], [{id: 3, name: "Vitamine nehmen", icon: "book", color: "#be4848", frequency: 10, entries: 0}, {id: 4, name: "Wasser trinken", icon: "glass", color: "#912929", frequency: 5, entries: 3}], [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#983f3f", frequency: 5, entries: 2}, {id: 6, name: "Sport machen", icon: "pill", color: "#d28787", frequency: 4, entries: 2}], [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#be4848", frequency: 5, entries: 5}, {id: 6, name: "Sport machen", icon: "pill", color: "#be4848", frequency: 4, entries: 4}]]
 
 </script>
 
