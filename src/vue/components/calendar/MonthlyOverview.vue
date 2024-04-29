c55ae12bb2fb80b43087658d23f2e1a6a577e287
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-29 12:50:01 +0200

    display the MonthlyTracker
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/calendar/MonthlyOverview.vue
+++ src/vue/components/calendar/MonthlyOverview.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 
+import MonthlyTracker from "./MonthlyTracker.vue";
+
 const currentDate = new Date();
 function getMonth(month?: number): Array<string|number>{
     if (!month) month = currentDate.getMonth();
@@ -16,6 +18,7 @@ function getMonth(month?: number): Array<string|number>{
 
 <template>
     <h2> {{ getMonth()[0]}}: {{currentDate.toLocaleDateString('sv')}} </h2>
+    <MonthlyTracker></MonthlyTracker>
 </template>
 
 <style scoped>
