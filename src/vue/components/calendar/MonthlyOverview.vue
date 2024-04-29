aadcc82743f77a27940aa579d3dd5b66668be2db
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-29 10:13:53 +0200

    display the current month
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/calendar/MonthlyOverview.vue
+++ src/vue/components/calendar/MonthlyOverview.vue
@@ -3,28 +3,14 @@ const currentDate = new Date();
 const currentMonth = currentDate.getMonth();
 
 function getMonthString(month: number) {
-    const months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-    ];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
     return months[month];
 }
 
 </script>
 
 <template>
-
-
+<h2> {{ getMonthString(currentMonth)}}</h2>
 </template>
 
 <style scoped>
