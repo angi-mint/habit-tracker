48f56c6547cfb11dcd2bf031a425e664426e6aab
FelixZipp <50374850+PanikLanguste25@users.noreply.github.com>
2024-05-02 09:32:44 +0200

    call showDailyHabits in the Daily Overview

--- src/vue/components/calendar/DailyOverview.vue
+++ src/vue/components/calendar/DailyOverview.vue
@@ -1,30 +1,60 @@
 <script setup lang="ts">
+import { onMounted, ref } from "vue";
 import DailyTracker from "./DailyTracker.vue";
 
-// get actual data from database later
-const data = [[{id: 1, name: "Wasser trinken", icon: "heart", color: "#e97a7a", frequency: 5, entries: 3}, {id: 2, name: "Buch lesen", icon: "pill", color: "#be4848", frequency: 4, entries: 1}], [{id: 3, name: "Vitamine nehmen", icon: "book", color: "#be4848", frequency: 10, entries: 0}, {id: 4, name: "Wasser trinken", icon: "glass", color: "#912929", frequency: 5, entries: 3}], [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#983f3f", frequency: 5, entries: 2}, {id: 6, name: "Sport machen", icon: "pill", color: "#d28787", frequency: 4, entries: 2}], [{id: 5, name: "Zimmer aufräumen", icon: "heart", color: "#be4848", frequency: 5, entries: 5}, {id: 6, name: "Sport machen", icon: "pill", color: "#be4848", frequency: 4, entries: 4}]]
+interface Habit {
+  id: number;
+  name: string;
+  icon: string;
+  color: string;
+  frequency: number;
+  entries: number;
+}
+
+interface HabitData {
+  daily: Habit[];
+  weekly: Habit[];
+  monthly: Habit[];
+  done: Habit[];
+}
+
+const data = ref<HabitData>({
+  daily: [],
+  weekly: [],
+  monthly: [],
+  done: [],
+});
+
+const fetchDailyHabits = async () => {
+  data.value = await window.api.getDailyHabits();
+};
+
+onMounted(async () => {
+  await fetchDailyHabits();
+  console.log(data.value);
+});
 
 </script>
 
 <template>
     <div class="habits-daily habits-wrapper">
         <h2>Heute</h2>
-        <DailyTracker v-for="habit in data[0]" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
+        <DailyTracker v-for="habit in data.daily" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
     </div>
 
     <div class="habits-weekly habits-wrapper">
         <h2>Diese Woche</h2>
-        <DailyTracker v-for="habit in data[1]" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
+        <DailyTracker v-for="habit in data.weekly" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
     </div>
 
     <div class="habits-monthly habits-wrapper">
         <h2>Diesen Monat</h2>
-        <DailyTracker v-for="habit in data[2]" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
+        <DailyTracker v-for="habit in data.monthly" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
     </div>
 
     <div class="habits-finished habits-wrapper">
         <h2>Erledigt!</h2>
-        <DailyTracker v-for="habit in data[3]" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
+        <DailyTracker v-for="habit in data.done" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
     </div>
 </template>
 
