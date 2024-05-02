a0f6b3777bb533a1301c3bd40a97046a84c87646
FelixZipp <50374850+PanikLanguste25@users.noreply.github.com>
2024-05-02 11:27:07 +0200

    add function addHabit and getOrAddCategory

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+import { Habit } from "./interface";
 
 function openDb() {
     const db = new sqlite3.Database("./src/database/habitdb.db", (err) => {
@@ -11,6 +12,77 @@ function openDb() {
     return db;
 }
 
+function getOrAddCategory(categoryName: string): Promise<number> {
+    const db = openDb();
+    return new Promise((resolve, reject) => {
+        // First, try to find the category
+        db.get('SELECT id FROM category WHERE name = ?', [categoryName], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (row) {
+                // If the category exists, resolve with its id
+                resolve(row.id);
+            } else {
+                // If the category does not exist, add it to the database
+                db.run('INSERT INTO category (name) VALUES (?)', [categoryName], function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        // Resolve with the id of the newly inserted category
+                        resolve(this.lastID);
+                    }
+                });
+            }
+        });
+    });
+}
+
+async function addHabit(habit: Habit): Promise<number> {
+    const db = openDb();
+    const categoryID = habit.category === undefined ? 5 : await getOrAddCategory(habit.category);
+
+    return new Promise((resolve, reject) => {
+        let sql: string;
+        let params: Array<string | number | boolean>;
+        if (habit.timeperiod) {
+            sql =
+                "INSERT INTO habit (name, frequency, interval, timeperiod, startDate, endDate, category_id, color_id, icon_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            params = [
+                habit.name,
+                habit.frequency,
+                habit.interval,
+                habit.timeperiod,
+                habit.startDate,
+                habit.endDate,
+                categoryID,
+                habit.color,
+                habit.icon
+            ];
+        } else {
+            sql =
+                "INSERT INTO habit (name, frequency, interval, timeperiod, category_id, color_id, icon_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            params = [
+                habit.name,
+                habit.frequency,
+                habit.interval,
+                habit.timeperiod,
+                categoryID,
+                habit.color,
+                habit.icon
+            ];
+        }
+
+        db.run(sql, params, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+}
+
 export default {
     openDb,
+    addHabit,
 };
