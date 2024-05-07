b58c6f7df2c705434558b3465749fb45a05d60c0
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-07 16:51:42 +0200

    quick fix
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -22,9 +22,9 @@ const createWindow = () => {
 
     // and load the index.html of the app.
     if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
-        mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+        void mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
     } else {
-        mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+        void mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
     }
 
     // Open the DevTools.
