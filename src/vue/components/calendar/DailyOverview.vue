16b93ccdd81ef1c4bf305d7fd2e62689143537ee
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-08 23:29:12 +0200

    hotfix
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/calendar/DailyOverview.vue
+++ src/vue/components/calendar/DailyOverview.vue
@@ -54,22 +54,25 @@ onMounted(async () => {
     <div class="habits-daily habits-wrapper">
         <h2>Heute</h2>
         <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.daily" :habit="habit"
-                      :interval="habit.entries + '/' + habit.frequency"></DailyTracker>
+                      :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
     </div>
 
     <div class="habits-weekly habits-wrapper">
         <h2>Diese Woche</h2>
-        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.weekly" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
+        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.weekly" :habit="habit"
+                      :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
     </div>
 
     <div class="habits-monthly habits-wrapper">
         <h2>Diesen Monat</h2>
-        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.monthly" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
+        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.monthly" :habit="habit"
+                      :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
     </div>
 
     <div class="habits-finished habits-wrapper">
         <h2>Erledigt!</h2>
-        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.done" :id="habit.id" :name="habit.name" :icon="habit.icon" :color="habit.color" :frequency="habit.frequency" :entries="habit.entries" :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
+        <DailyTracker @reload="handleReload(habit.id)" v-for="habit in data.done" :habit="habit"
+                      :interval="habit.entries + '/' + habit.frequency" :key="reloadKeys[habit.id]"></DailyTracker>
     </div>
 </template>
 
