0a8508838e0772ec828cf93fe87ab8f9ad738343
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-12 09:51:29 +0200

    connect to ical function
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/electron/ical.ts
+++ src/electron/ical.ts
@@ -1,12 +1,18 @@
 import db from "../database/database";
 
-async function fetchHabits() {
+async function connectToICal() {
     const { net } = require('electron');
     const creds = await db.getICalCredentials();
     if (!creds) return;
 
     const request = net.request(creds.url);
     request.setHeader('Authorization', 'Basic ' + Buffer.from(creds.username + ':' + creds.password).toString('base64'));
+    return request;
+}
+
+async function fetchHabits() {
+    const request = await connectToICal();
+    if (!request) return;
 
     request.on('response', (response) => {
         console.log(`STATUS: ${response.statusCode}`)
