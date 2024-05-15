f16f85186ab8de00826a5d19126f05366403c91b
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-15 20:30:35 +0200

    create single ical event
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/electron/ical.ts
+++ src/electron/ical.ts
@@ -1,4 +1,6 @@
 import db from "../database/database";
+import {Habit} from "../database/interface";
+import {ICalCalendar, ICalEventRepeatingFreq} from "ical-generator";
 
 async function connectToICal() {
     const { net } = require('electron');
@@ -22,6 +24,21 @@ async function fetchHabits() {
         });
     });
     request.end()
+}
+
+async function createCalendarEntry(cal: ICalCalendar, habit: Habit) {
+    const event = cal.createEvent({
+        start: new Date(habit.startDate),
+        end: new Date(habit.endDate),
+        summary: habit.name,
+        description: habit.category,
+    });
+    event.repeating({
+        freq: habit.interval === 0 ? ICalEventRepeatingFreq.DAILY : habit.interval === 1 ? ICalEventRepeatingFreq.WEEKLY : ICalEventRepeatingFreq.MONTHLY,
+        count: habit.frequency
+    });
+    event.x('X-HABIT-ID', habit.id.toString());
+    console.log(event);
 }
 
 export { fetchHabits }
