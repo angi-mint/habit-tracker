3bf641277791c822e71fb91f1fd238f4a97e0a40
PanikLanguste25 <50374850+PanikLanguste25@users.noreply.github.com>
2024-05-15 16:09:14 +0200

    add required attribute to color and icon radio inputs

--- src/vue/components/CreateHabit.vue
+++ src/vue/components/CreateHabit.vue
@@ -107,11 +107,11 @@ const onSubmit = async () => {
                         </template>
                     </LabelForm>
 
-                    <LabelForm>
+                    <LabelForm required>
                         <template #form-label><p>Farbe</p></template>
                         <template #input>
                             <label v-for="items in colors">
-                                <input type="radio" v-model="habitData.color" :value="items.id" :checked="Props.habitData?.color === items.name">
+                                <input type="radio" v-model="habitData.color" :value="items.id" :checked="Props.habitData?.color === items.name" name="color" required>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" :fill="items.name" viewBox="0 0 16 16">
                                     <circle cx="8" cy="8" r="8"/>
                                 </svg>
@@ -119,11 +119,11 @@ const onSubmit = async () => {
                         </template>
                     </LabelForm>
 
-                    <LabelForm>
+                    <LabelForm required>
                         <template #form-label><p>Icon</p></template>
                         <template #input>
                             <label v-for="items in icons">
-                                <input type="radio" v-model="habitData.icon" :value=items.id :checked="Props.habitData?.icon === items.name">
+                                <input type="radio" v-model="habitData.icon" :value=items.id :checked="Props.habitData?.icon === items.name" name="icon" required>
                                 <HabitIcon :id=items.name />
                             </label>
                         </template>
