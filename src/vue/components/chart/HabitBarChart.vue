398be2f525fd05062faf0138c10173bb785501b5
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-16 17:25:20 +0200

    generate Bar Chart
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/vue/components/chart/HabitBarChart.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {BarChart} from 'vue-chart-3';
+import {PropType} from "vue";
+import {Chart, registerables} from "chart.js";
+
+Chart.register(...registerables);
+
+const DataProps = defineProps({
+    interval: String,
+    labels: Array as PropType<Array<string>>,
+    data: Array as PropType<Array<number>>,
+    colors: Array as PropType<Array<string>>,
+});
+
+const HabitData = {
+    labels: DataProps.labels,
+    datasets: [{
+        label: '% der Erreichten Habits',
+        data: DataProps.data,
+        backgroundColor: DataProps.colors,
+    }]
+}
+
+</script>
+
+<template>
+    <BarChart :chartData="HabitData" />
+</template>
+
+<style scoped>
+
+</style>
