a09deef6b6d064f4df09a426d2d6a005b5c25c94
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-16 18:04:00 +0200

    get data from database
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/vue/components/chart/HabitChartOverview.vue
+++ src/vue/components/chart/HabitChartOverview.vue
@@ -1,27 +1,17 @@
 <script setup lang="ts">
 import HabitBarChart from "./HabitBarChart.vue";
 
-// const habitData = await window.api.invoke
-const habitData = [{id: 1, name: "Drink Water", icon: "water", color: "#b43737", frequency: 8, entries: 4, interval: "Daily", dates: ["2024-04-01", "2024-04-02", "2022-04-03", "2024-04-04"]}, {id: 2, name: "Read Book", icon: "book", color: "#bd6060", frequency: 2, entries: 1, interval: "Weekly", dates: ["2024-04-01", "2024-04-01", "2024-04-10", "2022-04-15", "2024-04-22"]},];
-
 const currentDay = new Date();
+const options = new Intl.DateTimeFormat('sv', {day: "2-digit", month: "2-digit", year: "numeric"});
 
-// Calculate the start date (4 weeks ago from the beginning of the current week)
+
 const startDate = new Date(currentDay);
-startDate.setDate(startDate.getDate() - (startDate.getDay() + 20)); // Go back to the beginning of the week 4 weeks ago
+startDate.setDate(startDate.getDate() - (startDate.getDay() + 20));
 
-// Calculate the end date (last day of the previous week)
 const endDate = new Date(currentDay);
-endDate.setDate(endDate.getDate() - (endDate.getDay() + 1)); // Go back to the end of the previous week
+endDate.setDate(endDate.getDate() - (endDate.getDay() + 1));
 
-// Format the dates (optional, for better readability)
-const formatDate = (date: Date) => {
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are zero-based
-    const year = date.getFullYear();
-    return `${day}-${month}-${year}`;
-};
-
+const habitData = await window.api.getWeeklyOrMonthlyHabits(options.format(startDate), options.format(endDate));
 
 </script>
 
